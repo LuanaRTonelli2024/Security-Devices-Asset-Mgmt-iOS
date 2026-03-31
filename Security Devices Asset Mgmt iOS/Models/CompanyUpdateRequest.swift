7c25df35d5a04b7d57f3d32a40bd5a3580c976a7
Luana R Tonelli <luana_tonelli@hotmail.com>
2026-03-31 00:16:53 -0400

    Add CompanyUpdateRequest struct for company updates

--- Security Devices Asset Mgmt iOS/Models/CompanyUpdateRequest.swift
+++ Security Devices Asset Mgmt iOS/Models/CompanyUpdateRequest.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct CompanyUpdateRequest: Codable {
+    let name: String
+    let address: String?
+    let contact: String?
+    init(from e: CompanyEntity) {
+        name = e.name ?? ""
+        address = e.address
+        contact = e.contact
+    }
+}
