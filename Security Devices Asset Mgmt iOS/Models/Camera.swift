74775cbdcb4caa0a6d5a7e4d1239e5aa231c5854
Luana R Tonelli <luana_tonelli@hotmail.com>
2026-03-31 00:14:09 -0400

    Add CameraDTO struct for camera data management

--- Security Devices Asset Mgmt iOS/Models/Camera.swift
+++ Security Devices Asset Mgmt iOS/Models/Camera.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct CameraDTO: Codable {
+    let id: String?
+    let name: String?
+    let location: String?
+    let ipAddress: String?
+    let subnetMask: String?
+    let defaultGateway: String?
+    let userName: String?
+    let password: String?
+    let companyId: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case name
+        case location
+        case ipAddress
+        case subnetMask
+        case defaultGateway
+        case userName
+        case password
+        case companyId
+    }
+}
