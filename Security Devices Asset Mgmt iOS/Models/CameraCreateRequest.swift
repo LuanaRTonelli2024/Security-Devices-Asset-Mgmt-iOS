2c674c65131302d659bc254543656e7f02c9c57f
Luana R Tonelli <luana_tonelli@hotmail.com>
2026-03-31 00:14:47 -0400

    Add CameraCreateRequest struct for camera creation

--- Security Devices Asset Mgmt iOS/Models/CameraCreateRequest.swift
+++ Security Devices Asset Mgmt iOS/Models/CameraCreateRequest.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct CameraCreateRequest: Codable {
+    let name: String
+    let location: String
+    let ipAddress: String
+    let subnetMask: String
+    let defaultGateway: String
+    let userName: String
+    let password: String
+    let companyId: String?
+    
+    init(from e: CameraEntity) {
+        name           = e.name ?? ""
+        location       = e.location ?? ""
+        ipAddress      = e.ipAddress ?? ""
+        subnetMask     = e.subnetMask ?? ""
+        defaultGateway = e.defaultGateway ?? ""
+        userName       = e.userName ?? ""
+        password       = e.password ?? ""
+        companyId      = e.companyId
+    }
+}
